5a2c7dfc2f625ba547a415d79a142e603b906530
Henrique <henriquebmx@gmail.com>
2023-01-06 14:00:02 -0300

    innitial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules : [
+        {
+          test: /\.scss$/,
+          use: [
+            'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    }
+  }
+}
