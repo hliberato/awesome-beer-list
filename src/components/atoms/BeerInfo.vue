66976bbade9ed52bebd726694001515c3e8d8aec
Henrique <henriquebmx@gmail.com>
2023-01-09 15:42:28 -0300

    create info component

---
+++ src/components/atoms/BeerInfo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="beer-info">
+    <div class="beer-info__properties">
+      IBU <br />
+      {{ beer.ibu }}<br /><br />
+      EBC <br />{{ beer.ebc }}
+    </div>
+    <img :src="beer.image_url" class="beer-info__image" />
+    <div class="beer-info__properties beer-info__properties--right">
+      ABV <br />
+      {{ beer.abv }}%<br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BeerInfo",
+  props: {
+    beer: {
+      type: Object,
+      default: () => {},
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.beer-info {
+  display: flex;
+  flex-direction: row;
+  &__properties {
+    width: 56px;
+    font-size: 0.7rem;
+  }
+  &__properties--right {
+    text-align: right;
+  }
+  &__image {
+    margin: auto;
+    height: 160px;
+    @media (max-width: 768px) {
+      max-width: 42px;
+    }
+  }
+}
+</style>
--- src/components/molecules/BeerCard.vue
+++ src/components/molecules/BeerCard.vue
@@ -1,17 +1,6 @@
 <template>
   <el-card class="beer-card" shadow="always">
-    <div class="beer-card__header">
-      <div class="beer-card__properties">
-        IBU <br />
-        {{ beer.ibu }}<br /><br />
-        EBC <br />{{ beer.ebc }}
-      </div>
-      <img :src="beer.image_url" class="beer-card__image" />
-      <div class="beer-card__properties beer-card__properties--right">
-        ABV <br />
-        {{ beer.abv }}%<br />
-      </div>
-    </div>
+    <beer-info :beer="beer" />
     <div class="beer-card__name">
       {{ beer.name }}
     </div>
@@ -22,6 +11,7 @@
 </template>
 
 <script>
+import BeerInfo from "../atoms/BeerInfo.vue";
 export default {
   name: "BeerCard",
   props: {
@@ -30,24 +20,15 @@ export default {
       default: () => {},
     },
   },
+  components: {
+    BeerInfo,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .beer-card {
-  background-color: #f2f2f2;
   margin-bottom: 24px;
-  &__header {
-    display: flex;
-    flex-direction: row;
-  }
-  &__properties {
-    width: 56px;
-    font-size: 0.7rem;
-  }
-  &__properties--right {
-    text-align: right;
-  }
   &__name {
     font-family: "Bitter", sans-serif;
     padding-top: 24px;
@@ -64,13 +45,6 @@ export default {
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-  }
-  &__image {
-    margin: auto;
-    height: 160px;
-    @media (max-width: 768px) {
-      max-width: 42px;
-    }
   }
 }
 </style>
